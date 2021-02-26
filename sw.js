d273edc78b0491c098de1ec17f3d624034da5a9d
Hendrik Thorun <hendrik.thorun@iteratec.com>
2021-02-26 10:43:14 +0100

    Updates

--- sw.js
+++ sw.js
@@ -27,23 +27,17 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-e2c0dad243ee6e6806fd.js"
-  },
-  {
-    "url": "styles.b05ecbb079ca2f327318.css"
-  },
-  {
-    "url": "styles-407fe62976dc5310c43e.js"
+    "url": "webpack-runtime-dbfce627d1f33d35ac80.js"
   },
   {
     "url": "framework-cd3e1e804d552fa282ef.js"
   },
   {
-    "url": "app-3537a5e69a7f95c51511.js"
+    "url": "app-0357331452569cb5b81e.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "f552226d5b8dcdaec25fb46d759db6da"
+    "revision": "ebe16969d3999032e3fff7f5c85871bd"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
@@ -57,7 +51,7 @@ self.__precacheManifest = [
   },
   {
     "url": "manifest.webmanifest",
-    "revision": "1d92f1c027bce9432b6f7d9787c8d202"
+    "revision": "d09227e4a4b631ba1f698a84fa19d407"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
@@ -149,7 +143,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-3537a5e69a7f95c51511.js`))) {
+  if (!resources || !(await caches.match(`/app-0357331452569cb5b81e.js`))) {
     return await fetch(event.request)
   }
 
