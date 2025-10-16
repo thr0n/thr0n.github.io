64f2103502ed6350b4f04272980bd1083c8e93c9
Hendrik Bölcke <hendrik.thorun@gmail.com>
2025-10-16 21:30:21 +0200

    Update gatsby.yml

--- cypress/e2e/landing-page.cy.ts
+++ cypress/e2e/landing-page.cy.ts
@@ -15,7 +15,6 @@ describe('Landing Page', () => {
   it('links to the desired external resources', () => {
     cy.get('header > div > div > a:first').should('have.attr', 'href').and('equal', 'https://github.com/thr0n')
     cy.get('header > div > div > a:nth-child(2)').should('have.attr', 'href').and('equal', 'https://www.linkedin.com/in/boelcke/')
-    cy.get('header > div > div > a:nth-child(3)').should('have.attr', 'href').and('equal', 'https://nerdculture.de/@thr0n')
   })
   
   it('shows the blog section', () => {
